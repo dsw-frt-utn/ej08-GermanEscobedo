96ca93603c9c782433a5258c7f48129638e2ea11
GermanEscobedo <Lautaro.Escobedo@alu.frt.utn.edu.ar>
2026-05-04 13:23:20 -0300

    se realiza problema 3

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Problema3
+    {
+        public string CompararCopias(int originalValue, Product product)
+        {
+            //hago una copia del valor
+            int copiaValor = originalValue;
+
+            //hago que solo se modifique la copia 
+            copiaValor++;
+
+            //hago copia de product
+            Product copiaProducto = product;
+
+            //se modifica la desc del producto 
+            copiaProducto.Description = "descripcion modificada";
+
+            return $"{originalValue}-{copiaValor}-{copiaProducto.Description}";
+        }
+
+    }
+}
