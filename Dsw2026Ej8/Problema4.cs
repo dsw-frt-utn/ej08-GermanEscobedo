175993a99473b789b713f18df78ef1a14bc80841
GermanEscobedo <Lautaro.Escobedo@alu.frt.utn.edu.ar>
2026-05-04 13:37:01 -0300

    se realiza problema 4

---
+++ Dsw2026Ej8/Problema4.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Problema4
+    {
+        public double CalcularPromedio(int? nota1, int? nota2, int? nota3)
+        {
+            int suma = 0;
+            int cantidadNotasValidas = 0;
+
+            if (nota1.HasValue && nota1.Value >= 0 && nota1.Value <= 10)
+            {
+                //se chequea si la nota 1 es valida
+                suma += nota1.Value;
+                cantidadNotasValidas++;
+            }
+            if (nota2.HasValue && nota2.Value >= 0 && nota2.Value <= 10)
+            {
+                //se chequea si la nota 2 es valida
+                suma += nota2.Value;
+                cantidadNotasValidas++;
+            }
+            if (nota3.HasValue && nota3.Value >= 0 && nota3.Value <= 10)
+            {
+                //se chequea si la nota 3 es valida
+                suma = nota3.Value;
+                cantidadNotasValidas++;
+            }
+            //si no hay notas validas
+            if (cantidadNotasValidas == 0)
+            {
+                return 0;
+            }
+
+            //Calcular el promedio con decimales
+
+            return (double) suma / cantidadNotasValidas;
+
+        }
+    }
+}
